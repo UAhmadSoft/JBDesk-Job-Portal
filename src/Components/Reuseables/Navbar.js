a6a061aa33959d7ce259f7fe8acd36f755ce65f1
U Ahmad <UmadAhmad1928@gmail.com>
2021-02-24 14:06:46 +0500

    Style : Fixed Navbar Styling

--- src/Components/Reuseables/Navbar.js
+++ src/Components/Reuseables/Navbar.js
@@ -19,8 +19,13 @@ import { withRouter } from 'react-router-dom';
 
 import Styles from '../../Styles/reuseables/Navbar.styles';
 
+//  color: #F2396E;
+//  border-bottom: 1px solid #F2396E;
+
 const Navbar = (props) => {
    const theme = useTheme();
+   const { location } = props;
+
    const styleProps = {
       ...props,
       theme,
@@ -35,6 +40,17 @@ const Navbar = (props) => {
    };
 
    const classes = Styles(styleProps);
+
+   const isActive = (slug) => {
+      console.log('slug', slug.toLowerCase());
+      console.log(
+         'location.pathName',
+         location.pathname.toLowerCase()
+      );
+      return location.pathname
+         .toLowerCase()
+         .includes(slug.toLowerCase());
+   };
 
    return (
       <>
@@ -51,42 +67,100 @@ const Navbar = (props) => {
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color:
+                           location.pathname === '/'
+                              ? theme.palette.pink
+                              : '#222',
+                        borderBottom:
+                           location.pathname === '/'
+                              ? `1px solid ${theme.palette.pink}`
+                              : '',
+                     }}
                   >
                      Home
                   </Typography>
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color: isActive('jobs')
+                           ? theme.palette.pink
+                           : '#222',
+                        borderBottom: isActive('jobs')
+                           ? `1px solid ${theme.palette.pink}`
+                           : '',
+                     }}
                   >
                      Jobs
                   </Typography>
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color: isActive('candidates')
+                           ? theme.palette.pink
+                           : '#222',
+                        borderBottom: isActive('candidates')
+                           ? `1px solid ${theme.palette.pink}`
+                           : '',
+                     }}
                   >
                      Candiates
                   </Typography>
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color: isActive('pages')
+                           ? theme.palette.pink
+                           : '#222',
+                        borderBottom: isActive('pages')
+                           ? `1px solid ${theme.palette.pink}`
+                           : '',
+                     }}
                   >
                      Pages
                   </Typography>
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color: isActive('dashboard')
+                           ? theme.palette.pink
+                           : '#222',
+                        borderBottom: isActive('dashboard')
+                           ? `1px solid ${theme.palette.pink}`
+                           : '',
+                     }}
                   >
                      Dashboard
                   </Typography>
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color: isActive('blog')
+                           ? theme.palette.pink
+                           : '#222',
+                        borderBottom: isActive('blog')
+                           ? `1px solid ${theme.palette.pink}`
+                           : '',
+                     }}
                   >
                      Blog
                   </Typography>
                   <Typography
                      variant='h6'
                      className={classes.LinkItem}
+                     style={{
+                        color: isActive('contact')
+                           ? theme.palette.pink
+                           : '#222',
+                        borderBottom: isActive('contact')
+                           ? `1px solid ${theme.palette.pink}`
+                           : '',
+                     }}
                   >
                      Contact
                   </Typography>

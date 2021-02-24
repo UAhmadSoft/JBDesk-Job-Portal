92a022b56bd37a75e3b9d0dd65ced4e078e0189f
U Ahmad <UmadAhmad1928@gmail.com>
2021-02-24 14:11:36 +0500

    Added Basic Home Page Setup

---
+++ src/Styles/HomeStyles.js
@@ -0,0 +1,6 @@
+import { makeStyles } from '@material-ui/core';
+import { genMediaQuery } from './reuseables/MediaQueries';
+
+const styles = makeStyles((styleProps) => ({}));
+
+export default styles;
